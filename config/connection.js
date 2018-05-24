e9a7855bad757b71706c096789de234eee75bc8c
Samantha Olvera <smolvera2010@yahoo.com>
2018-05-24 18:53:08 -0400

    removed security info

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ var Sequelize = require("sequelize");
   if (process.env.JAWSDB_URL) {
     var sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
-    sequelize = new Sequelize('briefcase_db', 'root', 'Malaya10@', {
+    sequelize = new Sequelize('database name', 'root', 'password', {
       host: 'localhost',
       dialect: 'mysql',
-      port: '3306'
+      port: 'port'
     })
   };
 
